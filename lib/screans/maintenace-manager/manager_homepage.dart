26631e455564ccd90580ac1d00084d2837c5338f
Sulaiman-Al-kadi <tu4102036@taibahu.edu.sa>
2024-05-11 00:28:14 +0300

    1.4

--- lib/screans/maintenace-manager/manager_homepage.dart
+++ lib/screans/maintenace-manager/manager_homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sanad_app/Routes.dart';
-import 'package:sanad_app/screans/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sanad_app/screans/navigation-bar/manager_nav_bar.dart';
 
 class ManagerHomepage extends StatefulWidget {
@@ -12,273 +12,187 @@ class ManagerHomepage extends StatefulWidget {
 }
 
 class _ManagerHomepageState extends State<ManagerHomepage> {
+  late String _firstName = '';
+  late String _lastName = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    // Get the current user
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      // Access the "users" collection in Firestore
+      DocumentSnapshot userData = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      // Retrieve first and last name from the document
+      setState(() {
+        _firstName = userData['firstName'] ?? '';
+        _lastName = userData['lastName'] ?? '';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(248, 249, 252, 1),
       appBar: AppBar(
-          shadowColor: Colors.grey,
-          toolbarHeight: 90,
-          backgroundColor: const Color(0xffFDFDFD),
-          leading: IconButton(
+        elevation: 0.2,
+        shadowColor: Colors.grey,
+        toolbarHeight: 90,
+        backgroundColor: const Color(0xffFDFDFD),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () {
+              print("object");
+            },
+            child: CircleAvatar(
+              radius: 30.0,
+              backgroundImage: AssetImage("images/تنزيل (3).jpeg"),
+              backgroundColor: Colors.transparent,
+            ),
+          ),
+        ),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Welcome,',
+              style: GoogleFonts.nunitoSans(
+                color: const Color.fromARGB(255, 85, 85, 85),
+                fontSize: 13,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            Text(
+              '$_firstName $_lastName',
+              style: GoogleFonts.nunitoSans(
+                color: const Color.fromARGB(255, 59, 59, 61),
+                fontSize: 17,
+                fontWeight: FontWeight.w900,
+              ),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
             padding: const EdgeInsets.all(10),
             onPressed: () {},
             icon: const Icon(Icons.notifications_none),
             iconSize: 37,
           ),
-          actions: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 10.0),
-                  child: Text(
-                    'مرحبا',
-                    style: GoogleFonts.nunitoSans(
-                      color: const Color.fromARGB(255, 85, 85, 85),
-                      fontSize: 15,
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10.0),
-                  child: Text('احمد محسن',
-                      style: GoogleFonts.nunitoSans(
-                        color: const Color.fromARGB(255, 59, 59, 61),
-                        fontSize: 17,
-                        fontWeight: FontWeight.w900,
-                      )),
-                )
-              ],
+        ],
+      ),
+      bottomNavigationBar: const ManagerNBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          textDirection: TextDirection.rtl,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10,
             ),
-            GestureDetector(
-              onTap: () {
-                return print("object");
-              },
-              child: const CircleAvatar(
-                radius: 35.0,
-                backgroundImage: AssetImage("images/تنزيل (3).jpeg"),
-                backgroundColor: Colors.transparent,
-              ),
-            ),
-          ]),
-      bottomNavigationBar:  ManagerNBar(),
-      body: Container(
-        width: MediaQuery.of(context).copyWith().size.width,
-        child: ListView(shrinkWrap: true, children: [
-          Container(
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                  image: AssetImage(
-                      "images/Screenshot_20240312_003406_WhatsApp.jpg"),
-                  fit: BoxFit.fill),
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.blue[300],
-            ),
-            height: 200,
-            width: double.maxFinite,
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Container(
-                margin: const EdgeInsets.all(10),
-                child: Container(
-                  height: 25,
-                )),
-            Container(
-              margin: const EdgeInsets.only(right: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
                 "الخدمات",
                 style: GoogleFonts.nunitoSans(
                   color: const Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 15,
+                  fontSize: 20,
                   fontWeight: FontWeight.w900,
                 ),
               ),
             ),
-          ]),
-          Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              padding: const EdgeInsets.all(5),
-              decoration: const BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Color(0xffF8F9FC),
-                )
-              ]),
-              height: 200,
-              width: 370,
-              child: PageView(
-                scrollDirection: Axis.horizontal,
-                controller: PageController(
-                  viewportFraction: 0.8,
-                  initialPage: 0,
+            SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: [
+                serviceButton(
+                  icon: Icons.category,
+                  title: " تصفح الطلبات",
+                  subTitle: "تصفح احدث البلاغات المرفوعة",
+                  routeName: "/mp_request_list",
                 ),
-                children: [
-                  // Text("page view1 "),
-                  // Text("page view2 "),
+                Divider(),
+                serviceButton(
+                  icon: Icons.location_on,
+                  title: " اضافة موظف",
+                  subTitle: "اضافة موظف الى قسم",
+                  routeName: "/manager_add_employee",
+                ),
+                Divider(),
+                serviceButton(
+                  icon: Icons.people,
+                  title: "الموظفين",
+                  subTitle: "رؤية معلومات الموظفين",
+                  routeName: "/manager_change_available",
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-                  GestureDetector(
-                    onTapDown: (details) {
-                      Navigator.pushNamed(context, "/mp_request_list",
-                          arguments: getCurrentUserId());
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Stack(
-                        alignment: AlignmentDirectional.center,
-                        textDirection: TextDirection.rtl,
-                        children: <Widget>[
-                          Container(
-                            clipBehavior: Clip.antiAlias,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(
-                                    15) // Adjust the radius as needed
-                                ),
-                            child: Image.asset(
-                              "images/eugen-str-CrhsIRY3JWY-unsplash.jpg",
-                              height: double.maxFinite,
-                              width: double.maxFinite,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text("تصفح الطلبات",
-                                  style: GoogleFonts.nunitoSans(
-                                    color: const Color.fromARGB(
-                                        255, 255, 255, 255),
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.w800,
-                                  )),
-                              Text("تصفح احدث البلاغات المرفوعة",
-                                  style: GoogleFonts.nunitoSans(
-                                    color: const Color.fromARGB(
-                                        255, 211, 210, 210),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w800,
-                                  )),
-                              Container(
-                                height: 15,
-                                width: 30,
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-
-                  GestureDetector(
-                    onTapDown: (details) {
-                      Navigator.pushNamed(context, "/manager_add_employee",
-                          arguments: getCurrentUserId());
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Stack(
-                        alignment: AlignmentDirectional.center,
-                        textDirection: TextDirection.rtl,
-                        children: <Widget>[
-                          Container(
-                            clipBehavior: Clip.antiAlias,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(
-                                    15) // Adjust the radius as needed
-                                ),
-                            child: Image.asset(
-                              "images/eugen-str-CrhsIRY3JWY-unsplash.jpg",
-                              height: double.maxFinite,
-                              width: double.maxFinite,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text("اضافة موظف ",
-                                  style: GoogleFonts.nunitoSans(
-                                    color: const Color.fromARGB(
-                                        255, 255, 255, 255),
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.w800,
-                                  )),
-                              Text("   اضافة موضف الى القسم",
-                                  style: GoogleFonts.nunitoSans(
-                                    color: const Color.fromARGB(
-                                        255, 211, 210, 210),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w800,
-                                  )),
-                              Container(
-                                height: 15,
-                                width: 30,
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-
-                   GestureDetector(
-                    onTapDown: (details) {
-                      Navigator.pushNamed(context, "/manager_change_available",
-                          arguments: getCurrentUserId());
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Stack(
-                        alignment: AlignmentDirectional.center,
-                        textDirection: TextDirection.rtl,
-                        children: <Widget>[
-                          Container(
-                            clipBehavior: Clip.antiAlias,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(
-                                    15) // Adjust the radius as needed
-                                ),
-                            child: Image.asset(
-                              "images/eugen-str-CrhsIRY3JWY-unsplash.jpg",
-                              height: double.maxFinite,
-                              width: double.maxFinite,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text("الموظفين",
-                                  style: GoogleFonts.nunitoSans(
-                                    color: const Color.fromARGB(
-                                        255, 255, 255, 255),
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.w800,
-                                  )),
-                              Text(" رؤية معلومات الموظفين",
-                                  style: GoogleFonts.nunitoSans(
-                                    color: const Color.fromARGB(
-                                        255, 211, 210, 210),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w800,
-                                  )),
-                              Container(
-                                height: 15,
-                                width: 30,
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-        ]),
+  Widget serviceButton({
+    required IconData icon,
+    required String title,
+    required String subTitle,
+    required String routeName,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(routeName);
+      },
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Container(
+          decoration: BoxDecoration(
+              // borderRadius: BorderRadius.circular(15),
+              color: Color(0xffFDFDFD)),
+          child: Card(
+            elevation: 0,
+            child: ListTile(
+              leading: Container(
+                  color: Colors.grey[300],
+                  child: Icon(
+                    icon,
+                    size: 50,
+                    color: Color.fromARGB(255, 15, 93, 156),
+                  )),
+              title: Text(
+                title,
+                style: GoogleFonts.nunitoSans(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 17,
+                  fontWeight: FontWeight.w800,
+                ),
+                textAlign: TextAlign.start,
+              ),
+              subtitle: Text(
+                subTitle,
+                style: GoogleFonts.nunitoSans(
+                  color: Colors.grey[500],
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
